77e8b993ed95f687ac3d017eca87055b487b849e
Timmy Tran <timmyhoangtranproj@gmail.com>
2019-06-13 15:35:25 -0700

    Go live!

---
+++ index.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let cardList = document.querySelector('#card-list')
+    let data
+    let game
+    let shell
+
+    axios.get('https://raw.githubusercontent.com/timmyhoangtranproj/proposal-example/master/g2ggames.json').then( function(result) {
+        data = result.data
+        for ( let i=0; i<data.length; i++) {
+            game = data[i]
+            shell = document.createElement('div')
+            shell.classList.add('card')
+            shell.innerHTML =  `
+            <div class="image">
+                <img src="${game.thumbnail}">
+            </div>
+            <div class="content">
+                <div class="header" style="text-align: center">
+                    ${game.name}
+                </div>
+                <div class="meta">
+                    &nbsp;&nbsp;&nbsp;&nbsp;${game.description}
+                </div>
+            </div>
+            <div class="extra content" style="text-align: center">
+                <a href="${game.url}" target="_blank">
+                    <button class="ui blue button">Get this free game</button>
+                </a>
+            </div>
+            `
+            cardList.appendChild(shell)
+        }
+    })
+})
+$('.top.menu .item').tab();
+hljs.initHighlightingOnLoad();
