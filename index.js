6f1bf4d359e464b85d7a6fd626da5df019d73f6c
Timmy Tran <timmyhoangtranproj@gmail.com>
2019-06-13 18:40:33 -0700

    Added Calculator tab

--- index.js
+++ index.js
@@ -32,5 +32,18 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 })
-$('.top.menu .item').tab();
-hljs.initHighlightingOnLoad();
+$('.top.menu .item').tab()
+hljs.initHighlightingOnLoad()
+
+function takeValue(x) {
+	document.getElementById('inputwindow').value += x
+}
+
+function clearInput(y) {
+	document.getElementById('inputwindow').value = y
+}
+
+function calculateResult() {
+	let result = eval(document.getElementById('inputwindow').value)
+	document.getElementById('inputwindow').value = result
+}
